df5c08851f1e3f782233b69908d9acab57489144
Richard Martin Zillmann <ri.zillmann@optadata-gruppe.de>
2024-08-09 09:38:36 +0200

    Id-Konstanten

---
+++ src/main/java/com/scuffed/jsonimporter/model/constant/IdConstants.java
@@ -0,0 +1,7 @@
+package com.scuffed.jsonimporter.model.constant;
+
+public final class IdConstants {
+	
+	public static final long BASE = 10000000000L;
+	public static final long PROCESS_RESERVE = 100L;
+}
